e2e410322a5f0a546eb2e5ac3a16847d6582b784
escii <escii@oom.ch>
2025-01-21 18:16:44 +0100

    fix name

--- src/snowfall/snowfall.php
+++ src/snowfall/snowfall.php
@@ -26,17 +26,17 @@ $updater->set_hooks();
 
 
 // Enqueue the CSS and JavaScript for the snowfall effect
-function christmas_snowfall_enqueue_scripts(): void
+function snowfall_enqueue_scripts(): void
 {
     wp_enqueue_style('snowfall-style', plugin_dir_url(__FILE__) . 'css/snowfall.css');
     wp_enqueue_script('snowfall-script', plugin_dir_url(__FILE__) . 'js/snowfall.js', array(), '1.0', true);
 }
-add_action('wp_enqueue_scripts', 'christmas_snowfall_enqueue_scripts');
+add_action('wp_enqueue_scripts', 'snowfall_enqueue_scripts');
 
 // Add the snowfall container to the footer
-function christmas_snowfall_footer(): void
+function snowfall_footer(): void
 {
     echo '<div id="snowfall-container"></div>';
 }
-add_action('wp_footer', 'christmas_snowfall_footer');
+add_action('wp_footer', 'snowfall_footer');
 
