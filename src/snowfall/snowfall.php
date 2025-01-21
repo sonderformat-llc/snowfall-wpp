b17aaf26c20e482432dff532d20bd043fd33d139
escii <escii@oom.ch>
2025-01-21 18:14:46 +0100

    updater code

--- src/snowfall/snowfall.php
+++ src/snowfall/snowfall.php
@@ -6,6 +6,25 @@ Version: 1.1
 Author: esc
 */
 
+// Plugin Updater
+require_once __DIR__ . 'class-updater-checker.php'; // Use your path to file
+
+use wpp\snowfall\Updater_Checker; // Use your namespace
+
+$github_username = 'escii'; // Use your gitbub username
+$github_repository = 'snowfall-wpp'; // Use your repository name
+$plugin_basename = plugin_basename( __FILE__ ); // Check note below
+$plugin_current_version = '1.0.0'; // Use the current version of the plugin
+
+$updater = new Updater_Checker(
+    $github_username,
+    $github_repository,
+    $plugin_basename,
+    $plugin_current_version
+);
+$updater->set_hooks();
+
+
 // Enqueue the CSS and JavaScript for the snowfall effect
 function christmas_snowfall_enqueue_scripts(): void
 {
